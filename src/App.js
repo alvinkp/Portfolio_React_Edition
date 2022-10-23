ca41514253aa20fbc65260c45253e9d731f46885
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-10-22 22:16:23 -0400

    Navbar items highlight
    *NavBar items are highlighted now depending on what page you are on*

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,11 +7,13 @@ import { About, Portfolio, Resume, Contact } from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Portfolio_React_Edition");
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
-        <Navbar />
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
         <Routes>
           <Route path="/Portfolio_React_Edition" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,47 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ currentPage, handlePageChange }) {
   return (
     <div className="d-flex flex-wrap justify-content-center">
       <nav className="navbar navbar-expand-lg">
-        <Link className="nav-link active fs-3" to="/Portfolio_React_Edition">
+        <Link
+          onClick={() => handlePageChange("Portfolio_React_Edition")}
+          className={
+            currentPage === "Portfolio_React_Edition"
+              ? "nav-link active fs-3"
+              : "nav-link fs-3"
+          }
+          to="/Portfolio_React_Edition"
+        >
           About Me
         </Link>
-        <Link className="nav-link active fs-3" to="/portfolio">
+        <Link
+          onClick={() => handlePageChange("portfolio")}
+          className={
+            currentPage === "portfolio"
+              ? "nav-link active fs-3"
+              : "nav-link fs-3"
+          }
+          to="/portfolio"
+        >
           Portfolio
         </Link>
-        <Link className="nav-link active fs-3" to="/resume">
+        <Link
+          onClick={() => handlePageChange("resume")}
+          className={
+            currentPage === "resume" ? "nav-link active fs-3" : "nav-link fs-3"
+          }
+          to="/resume"
+        >
           Resume
         </Link>
-        <Link className="nav-link active fs-3" to="/contact">
+        <Link
+          onClick={() => handlePageChange("contact")}
+          className={
+            currentPage === "contact" ? "nav-link active fs-3" : "nav-link fs-3"
+          }
+          to="/contact"
+        >
           Contact
         </Link>
       </nav>
