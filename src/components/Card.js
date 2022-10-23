619f4eb5f4018809623549e711a60ce007a2feb8
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-10-22 20:32:13 -0400

    Styled Css
    *Styled Background*
    
    *Established color scheme*
    
    *Updated Card Titles*
    
    *Added Google Fonts*

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,13 +8,13 @@ function Card(props) {
         className="card-img-top"
         alt={props.data.imgAltText}
       />
-      <div className="card-body">
+      <div className="card-body d-flex flex-column justify-content-between">
         <h5 className="card-title fs-2">{props.data.cardTitle}</h5>
-        <p className="card-text">{props.data.cardText}</p>
+        <p className="card-text fs-4">{props.data.cardText}</p>
 
         <a href={props.data.cardLink}>
           <button type="button" className="btn btn-primary">
-            Click Here
+            Click Here To Visit
           </button>
         </a>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ const portfolioCards = [
     imgLink: RigMatchMeLogo,
     imgAltText:
       "Screenshot of RigMatchMe site showing an over the shoulder picture of a drummer at a concert.",
-    cardTitle: "Rig Match Me",
+    cardTitle: "~ Rig Match Me ~",
     cardText:
       '"Rig Match Me", is where budding musicians go to find the best gear!',
     cardLink: "https://www.rigmatchme.com",
@@ -21,7 +21,7 @@ const portfolioCards = [
     imgLink: CodeMonstersLogo,
     imgAltText:
       "Code Monsters Logo featuring a one eyed monster with yellow fur a green eye and two horns, with a smile on its face",
-    cardTitle: "Code Monsters",
+    cardTitle: "~ Code Monsters ~",
     cardText:
       '"Code Monsters", is a website for new developers to find a plethora of video tutorials easily!',
     cardLink: "https://jdanleano.github.io/Code-Monsters/",
@@ -30,8 +30,8 @@ const portfolioCards = [
     id: 3,
     imgLink: EpicBrickBattleLogo,
     imgAltText:
-      "Screenshot of Epic Brick Battle Game showing a brick flying towards the screen.",
-    cardTitle: "Epic Brick Battle Game",
+      "Screenshot of Epic Brick Battle Game showing the arena where you will do battle!",
+    cardTitle: "~ Epic Brick Battle Game ~",
     cardText:
       '"Epic Brick Battle", is a free local single / multiplayer game where you pilot bricks and crash them into one another to see who will be the last brick standing!',
     cardLink: "https://alvinkpollard.itch.io/epic-brick-battle",
@@ -41,7 +41,7 @@ const portfolioCards = [
     imgLink: TheConcourseLogo,
     imgAltText:
       "The Concourse logo showing a stylized person running to the right.",
-    cardTitle: "The Concourse",
+    cardTitle: "~ The Concourse ~",
     cardText:
       '"The Concourse", is a highly competitive racing game that forces players to literally think on their feet. Use your wits and reflexes to conquer randomly generated courses while staying ahead of your opponents. Win and influence the future races by choosing a modifier, increasing the challenge!',
     cardLink: "https://store.steampowered.com/app/468780/The_Concourse/",
@@ -51,7 +51,7 @@ const portfolioCards = [
     imgLink: WIDSLogo,
     imgAltText:
       "West Indian Dominoes Simulator logo showing player avatars playing dominoes.",
-    cardTitle: "W.I.D.S",
+    cardTitle: "~ W.I.D.S ~",
     cardText:
       '"West Indian Dominoes Simulator", puts you in control of a robot that is learning how to play Dominoes like West Indians do. This game contains profanity from West Indian Countries. If you are not from the West Indies, the words you hear may not make sense because I had a particular audience in mind. Regardless, you can still play dominoes against 3 computer players.',
     cardLink: "https://alvinkpollard.itch.io/wids",
