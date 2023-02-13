22ad97b6fd1a071b52566932449d3b1d9ab547f3
alvinkp <102767990+alvinkp@users.noreply.github.com>
2023-02-13 11:10:49 -0500

    Updated RigMatchMe and BitsAndBytes Tech Blog to show Github Repo link only

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 // import { Link } from "react-router-dom";
 
 function Card(props) {
-  if (props.data.gitLink) {
+  if (props.data.gitLink && props.data.cardLink) {
     return (
       <div className='card'>
         <img
@@ -22,6 +22,31 @@ function Card(props) {
                 Visit Deployment
               </button>
             </a>
+            <a
+              href={props.data.gitLink}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button type='button' className='btn btn-primary'>
+                Visit Github Repo
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  } else if (props.data.id === 2 || props.data.id === 5) {
+    return (
+      <div className='card'>
+        <img
+          src={props.data.imgLink}
+          className='card-img-top'
+          alt={props.data.imgAltText}
+        />
+        <div className='card-body d-flex flex-column justify-content-between'>
+          <h5 className='card-title fs-2'>{props.data.cardTitle}</h5>
+          <p className='card-text'>{props.data.cardText}</p>
+          <div className='button-box'>
             <a
               href={props.data.gitLink}
               target='_blank'
